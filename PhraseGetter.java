f8e7a8780fd3324ad78ddf191fb7a684a8f1fe69
Greg Tomkins <tomkins.greg@gmail.com>
2016-11-14 21:34:19 -0800

    angular setup

--- PhraseGetter.java
+++ PhraseGetter.java
@@ -2,14 +2,19 @@ import com.google.gson.*;
 import java.io.*;
 
 public class PhraseGetter {
-	public static void main(String[] args) {
+	
+	public static Phrases get() {
+		Phrases phrases = null;
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("phrases.json"));
+			BufferedReader br = new BufferedReader(new FileReader("/tmp/phrases.json"));
 			Gson gson = new GsonBuilder().create();
-			Phrases phrases = gson.fromJson(br,  Phrases.class);
-			System.out.println("Hola Mundo! " + phrases.source);
-			phrases.phrases.forEach(item -> System.out.println(item.esRaw));
+			phrases = gson.fromJson(br,  Phrases.class);
+			return phrases;
+//			System.out.println("Hola Mundo! " + phrases.source);
+//			phrases.phrases.forEach(item -> System.out.println(item.esRaw));
 		} catch (Exception e) {
 		}
+		return null;
 	}
+	
 }
